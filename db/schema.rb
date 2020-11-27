ce6e5683434b667d658a910ed70453d6eee52263
MikelLiza <mikellizarralde4@gmail.com>
2020-11-27 12:06:12 -0800

    Added notes title index

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_27_200357) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_notes_on_title", unique: true
+  end
 
 end
